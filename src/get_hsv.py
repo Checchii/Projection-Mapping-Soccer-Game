9e0a2e90a210833cdcbcb6681015d9ba7df154f6
Sebastian Checchi <sebastian@Sebastians-MacBook-Pro-2.local>
2026-09-05 00:29:06 -0400

    Cleaned up comments

--- src/get_hsv.py
+++ src/get_hsv.py
@@ -1,26 +1,25 @@
 import cv2
 import numpy as np
 
-# List variable to store hsv pixel values
+# Array variable to store hsv pixel values
 vals = []
 
-# callback function
+# Callback function
 def on_mouse(event, x, y, flags, param):
-    # check if the event was the left mouse button being clicked
+    # Check if the event was the left mouse button being clicked
     if event == cv2.EVENT_LBUTTONDOWN:
-        # Get the BGR pixel value at the clicked location
-        # OpenCV represents RGB as BGR
+        # Get the BGR pixel value at the clicked location of the frame we pass
         pixel = param[y, x]
 
         # Convert BGR to HSV and print the pixel values
         hsv_pixel = cv2.cvtColor(np.uint8([[pixel]]), cv2.COLOR_BGR2HSV)
         print("HSV:", hsv_pixel[0][0])
 
-        # Append the pixel value to the value list
+        # Append the pixel value to the values array
         vals.append(hsv_pixel[0][0])
 
 def get_tresh_from_vals(vals: np.array) -> np.array:
-    # Calculate the min and max values for each channel
+    # Calculate the min and max values for each channel we'll need this for masking
     min_h, min_s, min_v = np.min(vals, axis=0)
     max_h, max_s, max_v = np.max(vals, axis=0)
 
